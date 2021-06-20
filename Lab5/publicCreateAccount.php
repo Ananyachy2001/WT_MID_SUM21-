25f2a221deb5d407bcf9b9549b2dd82569da1b4a
Ananya <Ananyachy2001@gmail.com>
2021-06-20 17:10:37 +0600

    Ananya's Forms added

---
+++ Lab5/publicCreateAccount.php
@@ -0,0 +1,57 @@
+<html>
+	<head></head>
+	<body>
+		<form action="" method="post">
+		<fieldset>
+            <h1 align="center">Create Account for People</h1>
+			<table>
+            <tr>
+					<td>Nid</td>
+					<td>: <input type="text" name="nid" value="" placeholder=""> </td>
+					
+				</tr>
+				<tr>
+					<td>Name</td>
+					<td>: <input type="text" name="name" value="" placeholder=""> </td>
+					
+				</tr>
+				<tr>
+					<td>Username</td>
+					<td>: <input type="text" name="username" value="" placeholder="">  </td>
+				
+				</tr>
+
+
+                <tr>
+					<td>Email</td>
+					<td>: <input type="text" name="email" placeholder="">  </td>
+                   
+					
+				</tr>
+                <tr>
+					<td>Password</td>
+					<td>: <input type="password" name="password" placeholder="Password">  </td>
+					
+				</tr>
+                <tr>
+					<td>Confirm Password</td>
+					<td>: <input type="password" name="conpassword" placeholder="Confirm Password">  </td>
+                    
+					
+				</tr>
+                <tr>
+					<td>Phone</td>
+					<td>: <input type="text"  name="phonecode" placeholder="Code"> - <input type="text" name="phonenumber" placeholder="Number">  </td>
+                   
+					
+				</tr>
+
+               
+
+			
+			
+			
+		</fieldset>
+		</form>
+	</body>
+</html>
